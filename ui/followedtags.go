050b665d6e0f4fde29bb168a719554b76d60ddaf
Paul Waldo <paul.w.waldo@gmail.com>
2023-05-04 19:28:57 -0400

    Adds buttons

--- ui/followedtags.go
+++ ui/followedtags.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/mattn/go-mastodon"
 )
@@ -10,6 +11,8 @@ import (
 type FollowedTagsUI struct {
 	KeepTags     []*mastodon.FollowedTag
 	RemoveTags   []*mastodon.FollowedTag
+	keepButton   *widget.Button
+	removeButton *widget.Button
 	container    *fyne.Container
 }
 
@@ -26,17 +29,30 @@ func (ui *FollowedTagsUI) SetFollowedTags(ft []*mastodon.FollowedTag) {
 }
 
 func (ui *FollowedTagsUI) MakeFollowedTagsUI() *fyne.Container {
+	ui.keepButton = widget.NewButtonWithIcon("Keep", theme.NavigateBackIcon(), func() {})
+	ui.removeButton = widget.NewButtonWithIcon("Remove", theme.NavigateNextIcon(), func() {})
+	ui.keepButton.Disable()
+	ui.removeButton.Disable()
+	buttons := container.NewVBox(ui.removeButton, ui.keepButton)
+
 	keepList := widget.NewList(
 		func() int {
 			return len(ui.KeepTags)
 		},
 		func() fyne.CanvasObject {
-			return widget.NewLabel("template")
+			return widget.NewLabel("XXXXXXXXXXXXXXX")
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
 			o.(*widget.Label).SetText(ui.KeepTags[i].Name)
-		})
+		},
+	)
+	keepList.OnSelected = func(id widget.ListItemID) {
+		ui.keepButton.Disable()
+		ui.removeButton.Enable()
+	}
+
 	keepLabel := widget.NewLabel("Tags to keep")
+	keepLabel.TextStyle = fyne.TextStyle{Bold: true}
 
 	removeList := widget.NewList(
 		func() int {
@@ -47,11 +63,19 @@ func (ui *FollowedTagsUI) MakeFollowedTagsUI() *fyne.Container {
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
 			o.(*widget.Label).SetText(ui.RemoveTags[i].Name)
-		})
+		},
+	)
+	removeList.OnSelected = func(id widget.ListItemID) {
+		ui.keepButton.Enable()
+		ui.removeButton.Disable()
+	}
+
 	removeLabel := widget.NewLabel("Tags to remove")
+	removeLabel.TextStyle = fyne.TextStyle{Bold: true}
 
 	keepBox := container.NewBorder(keepLabel, nil, nil, nil, keepList)
+	buttonBox := container.NewBorder(nil, nil, nil, nil, buttons)
 	removeBox := container.NewBorder(removeLabel, nil, nil, nil, removeList)
-	ui.container = container.NewHBox(keepBox, removeBox)
+	ui.container = container.NewHBox(keepBox, buttonBox, removeBox)
 	return ui.container
 }
